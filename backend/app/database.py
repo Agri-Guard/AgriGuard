96ea3806fb039527960be86613e0ea95c022743a
Keith Ndiema K <veritasndiema@gmail.com>
2026-07-21 18:48:37 +0300

    changes to the database.py and dashboard.py files
    changed the variables for the database

--- backend/app/database.py
+++ backend/app/database.py
@@ -9,16 +9,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-from backend.app.config import settings
+from backend.app.core.config import settings
 
 
 # Create database engine
 engine = create_engine(
-    settings.DATABASE_URL,
+    settings.database_url,
     pool_pre_ping=True,           # Helps detect dead connections
     pool_size=10,
     max_overflow=20,
-    echo=settings.DEBUG           # Set to True to see raw SQL queries (development only)
+    echo=settings.debug           # Set to True to see raw SQL queries (development only)
 )
 
 
